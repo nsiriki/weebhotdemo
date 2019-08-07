abba605e6a35cff40d0cfe46d794aec50ee3719d
Naresh <53210780+nsiriki@users.noreply.github.com>
2019-08-07 17:43:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
  <?php 
-
+ 
 $method = $_SERVER['REQUEST_METHOD'];
 
 // Process only when method is POST
 if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
-
+	
 	$speech = $json->queryResult->parameters->text;
-
+	
 	switch ($speech) {
 		case 'hi':
 			$text = "Hi, Nice to meet you";
 			break;
-
+			
 		case 'bye':
 			$text = "Bye, good night";
 			break;
-
+			
 		case 'anything':
 			$text = "Yes, you can type anything here.";
 			break;
@@ -26,18 +26,14 @@ if($method == 'POST'){
 			$text = "Sorry, I didnt get that. Please ask me something else.";
 			break;
 	}
-//$json->queryResult->fulfillmentMessages->text->text = "fgdfgdfgdg"
 
-	//$agent = \Dialogflow\WebhookClient::fromData($request->json()->all());
-	//$agent = WebhookClient::fromData($_POST);
-	//$intent = $agent->getIntent();
-	
 	$response = new \stdClass();
 	$response->speech = $text;
-	//$response->text = $agent->getIntent();
-	$response->queryResult->fulfillmentMessages->text->text = "webhook";
+	$response->displayText = $text;
+	$response->source = "webhook";
 	echo json_encode($response);
-	//echo json_encode($agent->render());
+	
+	
 }
 else
 {
@@ -45,5 +41,3 @@ else
 }
 
 ?> 
-
-
