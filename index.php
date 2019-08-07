3e9a3818cf007365e6d80082fe43770b2de9cddf
Naresh <53210780+nsiriki@users.noreply.github.com>
2019-08-07 12:30:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,23 +7,23 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
-	$text = $json->result->parameters->text;
+	$speech = $json->result->parameters->text;
 
-	switch ($text) {
+	switch ($speech) {
 		case 'hi':
-			$speech = "Hi, Nice to meet you";
+			$text = "Hi, Nice to meet you";
 			break;
 
 		case 'bye':
-			$speech = "Bye, good night";
+			$text = "Bye, good night";
 			break;
 
 		case 'anything':
-			$speech = "Yes, you can type anything here.";
+			$text = "Yes, you can type anything here.";
 			break;
 		
 		default:
-			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			$text = "Sorry, I didnt get that. Please ask me something else.";
 			break;
 	}
 
