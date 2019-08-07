96fccb43595e08990c0b13fa2a13601118310b6f
Naresh <53210780+nsiriki@users.noreply.github.com>
2019-08-07 12:38:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
-	$speech = $json->result->parameters->text;
+	$speech = $json->queryResult->parameters->text;
 
 	switch ($speech) {
 		case 'hi':
