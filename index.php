eae78b4b56e4c9b8262c4f02404d6fab693e6a15
Naresh <53210780+nsiriki@users.noreply.github.com>
2019-08-07 14:11:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -27,11 +27,11 @@ if($method == 'POST'){
 			break;
 	}
 
-	//$response = new \stdClass();
+	
+	$response = new \stdClass();
 	//$response->speech = $text;
 	//$response->text = $text;
-	//$response->queryResult->fulfillmentMessages->text->text = "webhook";
-	$json->queryResult->fulfillmentMessages->text->text = "Webhook"
+	$json->queryResult->fulfillmentMessages->text->text = "webhook";
 	echo json_encode($response);
 }
 else
